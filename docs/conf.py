31952d9f7177bb3c1ffe6bb61b7ba9ebc96cd1d8
Francesco Turini <fturini@localhost.localdomain>
2021-10-13 14:50:51 +0200

    eliminating surce directory in docs for solving problems in ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../Third_assignament_PDF/'))
+sys.path.insert(0, os.path.abspath('../Third_assignament_PDF/'))
 
 
 # -- Project information -----------------------------------------------------
